8264b5ae2981d91d402b42d0bf3e4cf2b913ba9c
Patrick Brunschwig <patrick@enigmail.net>
2020-07-11 19:02:32 +0200

    removed references to pEp, deleted icons and CSS related to pEp

--- package/prefs/defaultPrefs.js
+++ package/prefs/defaultPrefs.js
@@ -157,7 +157,7 @@ pref("extensions.enigmail.autoWkdLookup", 1);
 // 4: if send (un)encrypted due to rules
 pref("extensions.enigmail.confirmBeforeSending", 0);
 
-// Show or hide the Enigmail (or pEp) column in the message list
+// Show or hide the Enigmail column in the message list
 pref("extensions.enigmail.columnVisible", false);
 
 // Type of crypto-API to use - EXPERIMENTAL
@@ -208,15 +208,6 @@ pref("extensions.enigmail.warnDownloadContactKeys", true);
 
 // wrap HTML messages before sending inline PGP messages
 pref("extensions.enigmail.wrapHtmlBeforeSend", true);
-
-// automatically download pepmda if it is available (without askin user)
-pref("extensions.enigmail.pEpAutoDownload", true);
-
-// holds the timestamp of the last check for pEp updates
-pref("extensions.enigmail.pEpLastUpdate", 0);
-
-// automatically update pEp?
-pref("extensions.enigmail.pEpAutoUpdate", 0);
 
 // enable encryption/signing of headers like subject, from, to
 // 1: default: ask user at 1st time use / 0: off /  2: on
@@ -278,11 +269,9 @@ pref("mail.identity.default.defaultEncryptionPolicy", 0);
 pref("mail.identity.default.openPgpUrlName", "");
 pref("mail.identity.default.pgpMimeMode", true);
 pref("mail.identity.default.attachPgpKey", false);
-pref("mail.identity.default.attachPepKey", true);
 pref("mail.identity.default.autoEncryptDrafts", true);
 pref("mail.identity.default.protectSubject", true);
 pref("mail.identity.default.warnWeakReply", false);
-pref("mail.identity.default.enablePEP", true);
 
 /*
    Default pref values for the enigmail per-account
